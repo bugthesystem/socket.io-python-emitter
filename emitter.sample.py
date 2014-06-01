ed3abf77337ee6ab90b157ec2fa02c587990ecbe
ziyasal <sarikayaziya@gmail.com>
2014-06-01 06:28:11 -0700

    Added constructor options and emit tests

---
+++ emitter.sample.py
@@ -0,0 +1,18 @@
+import threading
+from emitter import Emitter
+
+__author__ = 'ziyasal'
+
+
+class AppWorker:
+    def __init__(self):
+        self.interval = 6000 * 1
+
+    def start(self):
+        io = Emitter({'host': 'localhost', 'port': 6379})
+        io.Emit('broadcast event', 'Hello from socket.io-emitter')
+        threading.Timer(self.interval, self.start).start()
+
+if __name__ == '__main__':
+    app = AppWorker()
+    app.start()
