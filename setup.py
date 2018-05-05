08b01dd0299b10a954caaca2428492c9601522e3
Abdullah Selek <abdullahselek@gmail.com>
2018-05-05 22:29:36 +0100

    More dynamic setup file.

--- setup.py
+++ setup.py
@@ -1,16 +1,55 @@
 #!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from __future__ import absolute_import, print_function
 
-from setuptools import setup
+import os
+import re
+import codecs
+
+from setuptools import setup, find_packages
+
+cwd = os.path.abspath(os.path.dirname(__file__))
+
+def read(filename):
+    with codecs.open(os.path.join(cwd, filename), 'rb', 'utf-8') as h:
+        return h.read()
+
+metadata = read(os.path.join(cwd, 'socket_io_emitter', '__init__.py'))
+
+def extract_metaitem(meta):
+    meta_match = re.search(r"""^__{meta}__\s+=\s+['\"]([^'\"]*)['\"]""".format(meta=meta),
+                           metadata, re.MULTILINE)
+    if meta_match:
+        return meta_match.group(1)
+    raise RuntimeError('Unable to find __{meta}__ string.'.format(meta=meta))
 
 setup(
     name='socket.io-emitter',
-    version='0.1.5',
-    author='Ziya SARIKAYA',
-    author_email='sarikayaziya@gmail.com',
-    packages=['socket_io_emitter'],
-    url='https://github.com/ziyasal/socket.io-python-emitter',
-    license='MIT',
-    description='A Python implementation of socket.io-emitter',
+    version=extract_metaitem('version'),
+    license=extract_metaitem('license'),
+    description=extract_metaitem('description'),
+    long_description=(read('README.rst')),
+    author=extract_metaitem('author'),
+    url=extract_metaitem('url'),
+    download_url=extract_metaitem('download_url'),
+    packages=find_packages(exclude=('tests', 'sample')),
+    platforms=['Any'],
     install_requires=['msgpack-python', 'redis'],
-    include_package_data=True
+    setup_requires=['pytest-runner'],
+    tests_require=['pytest'],
+    keywords='socket.io, emitter, pure python module, universal module',
+    include_package_data=True,
+    classifiers=[
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+    ],
 )
