89a6c79fb9469ed6e263504bd518c3075907b5b0
Abdullah Selek <abdullahselek@gmail.com>
2018-05-27 19:50:38 +0100

    Bump version.

--- socket_io_emitter/__init__.py
+++ socket_io_emitter/__init__.py
@@ -7,7 +7,7 @@ from __future__ import absolute_import
 __author__       = 'Ziya SARIKAYA'
 __copyright__    = 'Copyright 2014 Ziya SARIKAYA @ziyasal'
 __license__      = 'MIT License'
-__version__      = '0.1.5'
+__version__      = '0.1.5.1'
 __url__          = 'https://github.com/ziyasal/socket.io-python-emitter'
 __download_url__ = 'https://pypi.org/project/socket.io-emitter/'
 __description__  = 'Python implementation of socket.io-emitter.'
